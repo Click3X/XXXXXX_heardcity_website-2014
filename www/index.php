4e90001edbf5fe3cffcc583892e48b57a539f55e
Charles Howard Davis III <charles@click3x.com>
2014-03-03 16:23:07 -0500

    Echo variable

--- www/index.php
+++ www/index.php
@@ -4,4 +4,6 @@ include('functions.php');
   // header( 'Location: home.php' ) ;
 echo 'This is php and you are a BITCH BOY MAHN!';
 echo 'Current PHP version '.phpversion();
+
+echo print_r($address);
 ?>
