1fcaacdca7ed4d97c856a1025a53d1eae4d7e71e
Charles Howard Davis III <charles@click3x.com>
2014-03-03 16:33:25 -0500

    Check permissions

--- www/index.php
+++ www/index.php
@@ -2,6 +2,11 @@
 // include('php/config.php');
 // include('php/functions.php'); 
 
+$file ='php/functions.php';
+
+$read = is_readable($file);
+echo 'This is read: '.$read.', and this is file: '.$file;
+
   // header( 'Location: home.php' ) ;
 echo 'This is php and you are a BITCH BOY MAHN!';
 echo 'Current PHP version '.phpversion();
