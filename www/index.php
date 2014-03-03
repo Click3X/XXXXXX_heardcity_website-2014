2c7ecb393853c222bd6718a555de1ca7c73e6c18
Charles Howard Davis III <charles@click3x.com>
2014-03-03 16:22:25 -0500

    test file includes config and functions

--- www/index.php
+++ www/index.php
@@ -1,4 +1,6 @@
 <?php
+include('config.php');
+include('functions.php'); 
   // header( 'Location: home.php' ) ;
 echo 'This is php and you are a BITCH BOY MAHN!';
 echo 'Current PHP version '.phpversion();
