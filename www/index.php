75fc588e5abffc894092e019cb581540706f4313
Charles Howard Davis III <charles@click3x.com>
2014-03-03 16:23:43 -0500

    Update path for testing

--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php
-include('config.php');
-include('functions.php'); 
+include('php/config.php');
+include('php/functions.php'); 
   // header( 'Location: home.php' ) ;
 echo 'This is php and you are a BITCH BOY MAHN!';
 echo 'Current PHP version '.phpversion();
