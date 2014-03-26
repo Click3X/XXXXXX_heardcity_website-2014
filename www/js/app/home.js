58ee118f051192b37a8bf6513ed7cdb19f96bccd
Charles Howard Davis III <charles@click3x.com>
2014-03-26 14:41:40 -0400

    Featured vid slider shows video, though could be more efficient

--- www/js/app/home.js
+++ www/js/app/home.js
@@ -1,13 +1,19 @@
-define(["jquery", "util/helper", "bootstrap.min", "util/team-member-form", "util/carousel", "util/request-anim", "util/parallax" /*,"util/build-marquee"*/,"util/sticky-nav", "util/loader"], function($) {
+define(["jquery", "util/helper", "bootstrap.min", "util/team-member-form", "util/carousel", "util/request-anim", "util/parallax", "util/sticky-nav"], function($) {
 
 
     $(function() {
         var docBody = $('body'),
         home = docBody.hasClass('home');
-
         // Toggle helper * DELETE FOR PRODUCTION
         docBody.helper();
+    });
 
+    $('.open-vid').click(function(event) {
+    	console.log('I am open vid and I have been clicked!');
+    	event.preventDefault();
+    	var vidSib = $(this).siblings();
+    	console.dir(vidSib);
+    	vidSib.removeClass('hidden');
     });
 
 });
