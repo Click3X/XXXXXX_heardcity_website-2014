97d6d7cebbc56faf0581e55a950d095f5c1f7366
Charles Howard Davis III <charles@click3x.com>
2014-04-01 14:46:00 -0400

    Hover updates to Contact Page

--- www/contact.php
+++ www/contact.php
@@ -116,11 +116,13 @@ include('php/project-grid.php');
                 ?>
 
                 <div class="address">
-                    <ul>
+                    <ul class="address-info">
                         <li class="phone"><a href="tel:5551234567" class=""><?php echo $address['phone']; ?></a></li>
                         <li class="street"><a href="https://www.google.com/maps/place/16+W+22nd+St/@40.7412435,-73.9913197,17z/data=!4m2!3m1!1s0x89c259a386c20bd5:0x6da26b75635d4e84" target="_blank" class="g-map-link"><?php echo $address['street'].', '. $address['floor']; ?></a></li>
                         <!-- <li class="floor"><a href="https://www.google.com/maps/place/16+W+22nd+St/@40.7412435,-73.9913197,17z/data=!4m2!3m1!1s0x89c259a386c20bd5:0x6da26b75635d4e84" target="_blank" class="g-map-link"><?php // echo $address['floor']; ?></a></li> -->
                         <li class="city-li"><a href="https://www.google.com/maps/place/16+W+22nd+St/@40.7412435,-73.9913197,17z/data=!4m2!3m1!1s0x89c259a386c20bd5:0x6da26b75635d4e84" target="_blank" class="g-map-link"><?php echo $address['city']; ?></a></li>
+                    </ul>
+                    <ul class="contact-info">
                         <li class="name"><?php echo $contact['name']; ?></li>
                         <li class="email"><a href="mailto:<?php echo $contact['email']; ?>"><?php echo $contact['email']; ?></a></li>
                     </ul>
