eac53c99ece37b05bde55a6a6276fe647bf551f5
Charles Howard Davis III <charles@click3x.com>
2014-04-03 12:09:34 -0400

    Testing video fixed height / width

--- www/js/lib/util/grid.js
+++ www/js/lib/util/grid.js
@@ -351,7 +351,7 @@ var Grid = (function() {
 		create : function() {
 			// create Preview structure:
 			this.$title = $( '<li class="p-name"></li>' );
-			this.$videoTag = $('<video controls><source src="" type="video/mp4" /></video>');
+			this.$videoTag = $('<video controls height="480" width="640"><source src="" type="video/mp4" /></video>');
 
 			this.$director = $('<span class="p-director"></span>');
 			this.$directorList = $('<li><span class="bold">Directed by: </span></li>').append(this.$director);
