c02ce252087e1f3afdf0328ee9151bc97450254d
Charles Howard Davis III <charles@click3x.com>
2014-04-04 18:39:36 -0400

    Serve less images on our team page to mobile

--- www/js/lib/controller/sub-menu.js
+++ www/js/lib/controller/sub-menu.js
@@ -60,9 +60,8 @@ define(["jquery",
 
 		     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(navigator.userAgent) ) {
 	            device = 'mobile';
-	            // length = 40;
-	            length = teamMemberFactory.length;
-	            // console.log('this is device' + device);
+	            length = 40;
+	            // length = teamMemberFactory.length;
 	        } else {
 	            device = 'desk';
 	            // console.log('this is device' + device);
