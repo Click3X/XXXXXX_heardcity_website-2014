0b07ca3992b2b032ea85bfb5aa8870c6ea5b5658
Charles Howard Davis III <charles@click3x.com>
2014-04-07 12:25:52 -0400

    Marquee Slider is randomized

--- www/js/app/main.js
+++ www/js/app/main.js
@@ -198,30 +198,73 @@ define(['jquery',
         });
         $siteHeader.show();
 
-        // INITIALIZE SLIDERS
-        $marqueeSlider.royalSlider({
-            autoHeight: true,
-            transitionSpeed:1200,
-            arrowsNav: true,
-            arrowsNavAutoHide:false,
-            fadeinLoadedSlide: false,
-            controlNavigationSpacing: 0,
-            controlNavigation: 'none',
-            imageScaleMode: 'none',
-            imageAlignCenter:false,
-            slidesSpacing:0,
-            loop: true,
-            loopRewind:false,
-            numImagesToPreload: 6,
-            keyboardNavEnabled: true,
-            usePreloader: true,
-            autoPlay: {
-                // autoplay options go gere
-                enabled: true,
-                pauseOnHover: true,
-                delay:6000
-            }
+
+
+        // RANDOMIZE IMAGES before Marquee
+        $(function() {
+            $.fn.randomize = function(selector){
+                (selector ? this.find(selector) : this).parent().each(function(){
+                    $(this).children(selector).sort(function(){
+                        return Math.random() - 0.5;
+                    }).detach().appendTo(this);
+                });
+
+                return this;
+            };
+
+            $('#marquee-slider li').randomize();
+
+            // INITIALIZE SLIDERS
+            $marqueeSlider.royalSlider({
+                autoHeight: true,
+                transitionSpeed:1200,
+                arrowsNav: true,
+                arrowsNavAutoHide:false,
+                fadeinLoadedSlide: false,
+                controlNavigationSpacing: 0,
+                controlNavigation: 'none',
+                imageScaleMode: 'none',
+                imageAlignCenter:false,
+                slidesSpacing:0,
+                loop: true,
+                loopRewind:false,
+                numImagesToPreload: 6,
+                keyboardNavEnabled: true,
+                usePreloader: true,
+                autoPlay: {
+                    // autoplay options go gere
+                    enabled: true,
+                    pauseOnHover: true,
+                    delay:6000
+                }
+            });
+
         });
+
+        // INITIALIZE SLIDERS
+        // $marqueeSlider.royalSlider({
+        //     autoHeight: true,
+        //     transitionSpeed:1200,
+        //     arrowsNav: true,
+        //     arrowsNavAutoHide:false,
+        //     fadeinLoadedSlide: false,
+        //     controlNavigationSpacing: 0,
+        //     controlNavigation: 'none',
+        //     imageScaleMode: 'none',
+        //     imageAlignCenter:false,
+        //     slidesSpacing:0,
+        //     loop: true,
+        //     loopRewind:false,
+        //     numImagesToPreload: 6,
+        //     keyboardNavEnabled: true,
+        //     usePreloader: true,
+        //     autoPlay: {
+        //         // autoplay options go gere
+        //         enabled: true,
+        //         pauseOnHover: true,
+        //         delay:6000
+        //     }
+        // });
 
         $blackTextSlider.royalSlider({
             autoHeight: true,
