8b3917f61918df01cfe5100f2349694f192e940b
Charles Howard Davis III <charles@click3x.com>
2014-04-16 10:30:25 -0400

    Our Team page contained in 'self'

---
+++ mobile/self/index.php
@@ -0,0 +1,118 @@
+<?php 
+require_once('php/config.php');
+require_once('php/functions.php');
+$page = 'Our Team';
+$class = formatLink($page);
+?>
+<!DOCTYPE HTML>
+<html>
+	<head> 
+		<title>Heard City | <?php echo $page; ?></title> 
+		<link rel="shortcut icon" href="favicon.ico">
+		<meta charset="utf-8">
+
+        <meta name="viewport" content="width=device-width, 
+            initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, 
+            user-scalable=yes">	
+
+		<link rel="stylesheet" href="css/jquery-mobile.css" />
+		<link rel="stylesheet" href="css/lazyload-animate.css" />
+
+		<link rel="stylesheet" href="css/cluetip/jquery.cluetip.css" />
+		<link rel="stylesheet" href="css/cluetip.css" />
+
+		<style>
+		.sidebar-button-holder {
+			display: block; margin-top: 0;
+			width: 60px; height: 100%;
+			position: absolute;
+			right: 0; top: 0;
+			background: white;
+			border-left: 1px solid rgb(89, 90, 92); border-top: 0; border-bottom: 0; border-right: 0;
+			border-radius: 0;
+
+			text-shadow: none;
+			color: #222;
+			font-weight: 400;
+		}
+
+
+		</style>
+
+
+		<script>
+	        // TEST FOR MOBILE DEVICE / TABLET
+	        var device;
+	        if( /Android|webOS|iPhone|iPod|BlackBerry/i.test(navigator.userAgent) ) {
+	            device = 'mobile';
+	        } else {
+	            device = 'desk';
+	        }
+
+	    </script>
+
+		<script src="js/lib/jquery-1.9.1.js"></script>
+		<script src="js/lib/jquery-mobile.js"></script>
+		<script src="js/lib/lazyloadxt.js"></script>
+		<script src="js/lib/underscore-min.js"></script>
+		<script src="js/lib/jquery.hoverIntent.js"></script>
+		<script src="js/lib/jquery.cluetip.js"></script>
+	
+	</head>
+	<body class="<?php echo $class; ?>">
+		<div data-role="page" id="menu" data-theme="b">
+
+			<!-- TOP OVERLAY -->
+			<div data-role="panel" data-dark-modal="true" data-position="top" data-display="overlay" id="toppaneloverlay" data-theme="a">
+				<div data-role="navbar">
+					<?php include('php/nav-template.php'); ?>
+				</div><!-- /navbar -->
+			</div><!-- /panel -->
+			
+
+			<!-- SIDEBAR -->
+			<div data-role="panel" data-position="right" data-display="overlay" id="rightpanel" data-theme="a">
+				<?php include('php/sub-nav.php'); ?>
+			</div>
+
+			
+			<!-- HEADER -->
+			<div data-role="header" data-position="fixed">
+				<a href="#toppaneloverlay" data-role="button" data-rel="panel" data-dismissible="true">Open top overlay</a>
+				<h1><?php echo $page; ?></h1>
+			</div><!-- /header --> 
+	
+
+			<!-- CONTENT -->
+			<div data-role="content">	
+				<!-- SIDEBAR LINK -->
+				<a id="sidebar-button-holder" class="sidebar-button-holder" href="#rightpanel" data-role="button" data-rel="panel" data-dismissible="true">&lt;</a>
+
+				<!-- BIO -->
+				<?php include('php/ourteam/bio.php'); ?>
+				
+
+			</div><!-- /content -->
+			
+			<div data-role="footer"><!-- data-position="fixed" -->
+				<?php include('php/footer.php'); ?>
+			</div>
+		</div><!-- /page menu  -->
+		<script src="js/ourteam/functions.js"></script>
+		<script src="js/ourteam/index.js"></script>
+
+		<script>
+	    $(window).on('ajaxComplete', function() {
+
+	        setTimeout(function() {
+	            $(window).lazyLoadXT();
+	        }, 50);
+
+	        $.lazyLoadXT.onload.addClass = 'animated fadeIn';
+
+	    });
+	    </script>
+
+	</body>
+</html>
+
---
+++ mobile/self/php/nav-template.php
@@ -0,0 +1,18 @@
+<?php // NAV TEMPLATE ?>
+
+<!-- <nav id="mobile" data-role="navbar">
+    <div id="toggle-bar">
+        <a class="navicon mtoggle" href="#" data-role="button" data-dismissible="true">☰</a>
+        <a class="navicon mtoggle center-toggle" href="#"><span id="page-title" class="page-title"><?php echo $page; ?></span></a>
+    </div>		 -->     
+
+    <!-- <ul id="mmenu"> -->
+    <ul id="new-mmenu">
+        <li id="page-home"><p><a href="home.php" rel="external">Home</a></p></li>
+        <li id="page-ourteam"><p><a href="ourteam.php" rel="external">Our Team</a></p></li>
+        <li id="page-projects"><p><a href="projects.php" rel="external">Projects</a></p></li>
+        <li id="page-contact"><p><a href="contact.php" rel="external">Contact</a></p></li>
+    </ul>
+
+<!-- </nav> -->
+
