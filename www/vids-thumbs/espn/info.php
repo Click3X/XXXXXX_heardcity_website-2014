605294758ac72b92bd200f3926419dcf728362be
Charles Howard Davis III <charles@click3x.com>
2014-05-05 15:23:13 -0400

    Added ESPN video

---
+++ www/vids-thumbs/espn/info.php
@@ -0,0 +1,10 @@
+<?php // info
+
+$info = array(
+	'client' => 'ESPN',
+	'title' => 'World Cup 2014',
+	'agency' => 'Wieden + Kennedy',
+	'type' => 'commercial'
+	);
+
+?>
--- www/vids-thumbs/google/info.php
+++ www/vids-thumbs/google/info.php
@@ -3,8 +3,8 @@
 $info = array(
 	'client' => 'Google',
 	'title' => 'We\'re All Storytellers',
-	// 'director'=>'Merkley & Partners',
-	// 'mixer'=> 'Hope Floats',
 	'agency' => 'Google',
 	'type' => 'commercial'
 	);
+
+?>
