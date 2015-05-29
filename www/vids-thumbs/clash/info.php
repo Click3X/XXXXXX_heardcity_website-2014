a8d14ab447679a3aa5aef6e50489a5c768ddf64c
Wen Zhang <wen@click3x.com>
2015-05-29 11:24:08 -0400

    add video files

---
+++ www/vids-thumbs/clash/info.php
@@ -0,0 +1,10 @@
+<?php // info
+
+$info = array(
+	'client' => 'Clash of Clans',
+	'title' => 'Revenge',
+	// 'director'=>'Merkley & Partners',
+	// 'mixer'=> 'Hope Floats',
+	'agency' => 'Barton F. Graf 9000',
+	'type' => 'commercial'
+	);
